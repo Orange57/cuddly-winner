97812fc8c2bfb62b8d16c7a6df7cbc1ae8cfdb83
William Vasselin <william.vasselin@gmail.com>
2021-12-07 15:34:22 +0100

    appel dans un appel

--- lib/components/tasks/task_preview.dart
+++ lib/components/tasks/task_preview.dart
@@ -1,20 +1,111 @@
+import 'package:flutter/material.dart';
 // second route
-/*class Task_Preview extends StatelessWidget{
-    const SecondRoute({Key? key}) : super(key: key);
+class Task_Preview extends StatefulWidget {
+  const Task_Preview({Key? key, required this.title}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Task Preview'),
-            ),
-            body: Center(
-                child : ElevatedButton(
-                    child: Text('Go back'),
-                    onPressed: () {
-                        Navigator.pop(context);
-                    }
-                )
-            )
-        );
-}*/
+  final String title;
+
+  @override
+  State<Task_Preview> createState() => _Task_Preview();
+}
+
+class _Task_Preview extends State<Task_Preview>{
+  Widget oneTask = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /*1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /*2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Nom de la tâche',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Description',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
+        ),
+        /*3*/
+        const DoneWidget(),
+      ],
+    ),
+  );
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TO-DO Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('TO-DO Application'),
+        ),
+        body: ListView(
+          children: [
+            oneTask,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DoneWidget extends StatefulWidget {
+  const DoneWidget({Key? key}) : super(key: key);
+
+  @override
+  _DoneWidgetState createState() => _DoneWidgetState();
+}
+
+class _DoneWidgetState extends State<DoneWidget> {
+  bool _isDone = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isDone
+                ? const Icon(Icons.check_box_outlined)
+                : const Icon(Icons.check_box_outline_blank_outlined)),
+            color: Colors.green[500],
+            onPressed: _toggleDoneTask,
+          ),
+        ),
+        /*SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('Validation'),
+          ),
+        ),*/
+      ],
+    );
+  }
+
+  void _toggleDoneTask() {
+    setState(() {
+      if (_isDone) {
+        _isDone = false;
+      } else {
+        _isDone = true;
+      }
+    });
+  }
+}
--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/tasks/task_preview.dart';
 
 class AllTasks extends StatefulWidget {
   const AllTasks({Key? key, required this.title}) : super(key: key);
@@ -9,113 +10,17 @@ class AllTasks extends StatefulWidget {
   State<AllTasks> createState() => _AllTasksState();
 }
 
-class DoneWidget extends StatefulWidget {
-  const DoneWidget({Key? key}) : super(key: key);
-
-  @override
-  _DoneWidgetState createState() => _DoneWidgetState();
-}
-
-class _DoneWidgetState extends State<DoneWidget> {
-  bool _isDone = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isDone
-                ? const Icon(Icons.check_box_outlined)
-                : const Icon(Icons.check_box_outline_blank_outlined)),
-            color: Colors.green[500],
-            onPressed: _toggleDoneTask,
-          ),
-        ),
-        /*SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('Validation'),
-          ),
-        ),*/
-      ],
-    );
-  }
-
-  void _toggleDoneTask() {
-    setState(() {
-      if (_isDone) {
-        _isDone = false;
-      } else {
-        _isDone = true;
-      }
-    });
-  }
-}
-
 class _AllTasksState extends State<AllTasks> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
 
-    Widget oneTask = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Nom de la tâche',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Description',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          const DoneWidget(),
-        ],
-      ),
-    );
-
     return MaterialApp(
       title: 'TO-DO Application',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TO-DO Application'),
-        ),
-        body: ListView(
-          children: [
-            oneTask
-          ],
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const Task_Preview(title: 'TO-DO Application'),
     );
   }
 }
