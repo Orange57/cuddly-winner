de4b7cc0707f44f2513c0e29d72f4224e4c3f920
William Vasselin <william.vasselin@gmail.com>
2021-12-07 14:39:44 +0100

    tttt

--- lib/main.dart
+++ lib/main.dart
@@ -5,56 +5,6 @@ void main() {
   runApp(const ToDoList());
 }
 
-class DoneWidget extends StatefulWidget {
-  const DoneWidget({Key? key}) : super(key: key);
-
-  @override
-  _DoneWidgetState createState() => _DoneWidgetState();
-}
-
-class _DoneWidgetState extends State<DoneWidget> {
-  bool _isDone = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(0),
-          child: IconButton(
-            padding: const EdgeInsets.all(0),
-            alignment: Alignment.centerRight,
-            icon: (_isDone
-                ? const Icon(Icons.check_box_outlined)
-                : const Icon(Icons.check_box_outline_blank_outlined)),
-            color: Colors.green[500],
-            onPressed: _toggleDoneTask,
-          ),
-        ),
-        /*SizedBox(
-          width: 18,
-          child: SizedBox(
-            child: Text('Validation'),
-          ),
-        ),*/
-      ],
-    );
-  }
-
-  void _toggleDoneTask() {
-    setState(() {
-      if (_isDone) {
-        _isDone = false;
-      } else {
-        _isDone = true;
-      }
-    });
-  }
-}
-
-//
-
 class ToDoList extends StatelessWidget {
 
   const ToDoList({Key? key}) : super(key: key);
@@ -62,41 +12,7 @@ class ToDoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
-
-    Widget oneTask = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Nom de la tâche',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Description',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          const DoneWidget(),
-        ],
-      ),
-    );
-
+ 
     return MaterialApp(
       title: 'TO-DO Application',
       home: Scaffold(
--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -5,8 +5,90 @@ class AllTasks extends StatefulWidget {
 
   final String title;
 
+  Widget oneTask = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Nom de la tâche',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Description',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          const DoneWidget(),
+        ],
+      ),
+    );
+  
   @override
   State<AllTasks> createState() => _AllTasksState();
+}
+
+class DoneWidget extends StatefulWidget {
+  const DoneWidget({Key? key}) : super(key: key);
+
+  @override
+  _DoneWidgetState createState() => _DoneWidgetState();
+}
+
+class _DoneWidgetState extends State<DoneWidget> {
+  bool _isDone = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isDone
+                ? const Icon(Icons.check_box_outlined)
+                : const Icon(Icons.check_box_outline_blank_outlined)),
+            color: Colors.green[500],
+            onPressed: _toggleDoneTask,
+          ),
+        ),
+        /*SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('Validation'),
+          ),
+        ),*/
+      ],
+    );
+  }
+
+  void _toggleDoneTask() {
+    setState(() {
+      if (_isDone) {
+        _isDone = false;
+      } else {
+        _isDone = true;
+      }
+    });
+  }
 }
 
 class _AllTasksState extends State<AllTasks> {
