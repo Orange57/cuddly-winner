5a29cb25d30e41fd8dd4f7718f31b9d1cd56d502
William Vasselin <william.vasselin@gmail.com>
2021-12-07 14:29:12 +0100

    rename titleSection

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class ToDoList extends StatelessWidget {
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
 
-    Widget titleSection = Container(
+    Widget oneTask = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
         children: [
@@ -105,7 +105,7 @@ class ToDoList extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            titleSection,
+            oneTask,
           ],
         ),
       ),
