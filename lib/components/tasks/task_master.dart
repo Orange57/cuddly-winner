d2e6820ccb9b4de3aa2365f0c990ffcc23dc9549
William Vasselin <william.vasselin@gmail.com>
2021-12-08 09:22:28 +0100

    pute list_preview

--- lib/components/tasks/task_master.dart
+++ lib/components/tasks/task_master.dart
@@ -1,39 +1,18 @@
 import 'package:flutter/material.dart';
 import 'task_preview.dart';
-import '../../data/tasks.dart';
 
 class Task_Manage extends StatelessWidget {
   //
-
-  final tasks = List<Task>.generate(
-    15, (int index) {
-    return Task(
-        id: index,
-        content: 'Name task'.toString()
-    );}
-  );
+  final taskPreview = Task_Preview() ;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TO-DO Application',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TO-DO Application'),
-        ),
-        body: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: tasks.length,
-          itemBuilder: (context, index) {
-            final item = tasks[index];
-
-            return ListTile(
-              title: item.getContent(),
-              subtitle: item.getId(),
-            );
-          },
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: taskPreview,
     );
   }
 }
